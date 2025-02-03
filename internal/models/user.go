9fe2a5edd9855a13254a61333bbb1a728fa1e28f
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2025-02-03 18:37:42 +0300

    Add external pkgs

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       int    `json:"id,omitempty" validate="numeric"`
+	ID       int    `json:"id,omitempty"`
 	Name     string `json:"name,omitempty" validate="required,alphanum"`
 	Email    string `json:"email,omitempty" validate="required,email`
 	Password string `json:"password,omitempty" validate="required,min=6,max=64,printascii"`
