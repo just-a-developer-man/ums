1a28866ba3f0acadfc9db5bc00723ebdb2cddd34
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2025-02-06 13:06:48 +0300

    Change ID type to UUID

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,10 @@
 package models
 
+import "github.com/google/uuid"
+
 type User struct {
-	ID       int    `json:"id,omitempty" validate="numeric"`
-	Name     string `json:"name,omitempty" validate="required,alphanum"`
-	Email    string `json:"email,omitempty" validate="required,email`
-	Password string `json:"password,omitempty" validate="required,min=6,max=64,printascii"`
+	ID       uuid.UUID `json:"id,omitempty" validate:"uuid_rfc4122"`
+	Name     string    `json:"name,omitempty" validate:"required,alphanum"`
+	Email    string    `json:"email,omitempty" validate:"required,email"`
+	Password string    `json:"password,omitempty" validate:"required,min=6,max=64,printascii"`
 }
