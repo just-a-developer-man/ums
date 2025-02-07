c6146f54d7ac452c266e5251cecf62b40c19d151
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2025-02-07 12:33:27 +0300

    Extended user model with additional fields, add roles for users

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,40 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
 
+	"github.com/google/uuid"
+)
+
+// Role is a custom type for user roles.
+type Role string
+
+// Predefined roles
+const (
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
+)
+
+// User represents a user in the system.
 type User struct {
-	ID       uuid.UUID `json:"id,omitempty" validate:"uuid_rfc4122"`
-	Name     string    `json:"name,omitempty" validate:"required,alphanum"`
-	Email    string    `json:"email,omitempty" validate:"required,email"`
-	Password string    `json:"password,omitempty" validate:"required,min=6,max=64,printascii"`
+	// ID is the unique identifier of the user. It uses UUID format.
+	ID uuid.UUID
+
+	// Name is the user's name.
+	Name string
+
+	// Email is the user's email address.
+	Email string
+
+	// HashedPassword is the hashed version of the user's password.
+	HashedPassword string
+
+	// Role defines the user's role in the system (e.g., "admin" or "user").
+	Role Role
+
+	// CreatedAt is the timestamp when the user was created.
+	CreatedAt time.Time
+
+	// UpdatedAt is the timestamp when the user was last updated.
+	UpdatedAt time.Time
 }
